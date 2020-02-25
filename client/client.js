c1bff331401eb287c4ca50d31396b740a1c7c123
caasinehc <31968674+caasinehc@users.noreply.github.com>
2020-02-24 21:11:21 -0500

    Lots of stuff

--- client/client.js
+++ client/client.js
@@ -2,3 +2,42 @@
  * client.js
  * see main.js for full header
  */
+
+// Get this user's information
+const USERNAME = ice.math.random(["Alice", "Bob", "Charlie", "Eve", "John", "Jane"]) + " " + ice.math.random(["Chen", "Nguyen", "Smith", "Brown", "Doe"]);
+
+// Set up socket communication with the server
+// TODO Don't hardcode this
+const socket = io.connect("http://localhost:3000");
+socket.on("chatMsg", function(data) {
+	newChatMsg(data.msg, data.username);
+});
+socket.on("rawMsg", function(data) {
+	rawMsg(data.text);
+});
+
+// Server communication
+function newChatMsg(msg, username) {
+	rawMsg(`<${username}> ${msg}`);
+}
+function rawMsg(text) {
+	const elem = ice.dom.createP(text);
+	elem.classList.add("msg");
+	ice.dom.append(elem);
+}
+
+function sendChatMsg(msg) {
+	if(typeof USERNAME !== "string") {
+		console.error("sendChatMsg() error: USERNAME must be a string!");
+		return;
+	}
+	if(typeof msg !== "string") {
+		console.error("sendChatMsg() error: msg must be a string!");
+		return;
+	}
+	
+	socket.emit("chatMsg", {
+		username: USERNAME,
+		msg: msg
+	});
+}
--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
-	console.log(`Listening on port: ${PORT}`)
+	console.log(`Listening on port: ${PORT}`);
 });
 // Server by default serves static content in the "client" folder
 app.use(express.static("client"));
@@ -18,3 +18,42 @@ app.use(express.static("client"));
 app.use(express.json({
 	limit: "1mb"
 }));
+
+// Set up sockets
+const socket = require("socket.io");
+const io = socket(server);
+
+// When a new socket connects
+io.sockets.on("connection", function(socket) {
+	socket.on("chatMsg", function(data) {
+		const msg = data.msg;
+		const username = data.username;
+		if(typeof msg !== "string") {
+			console.log("Received invalid chatMsg data! data.msg was not a string!");
+			return;
+		}
+		if(typeof username !== "string") {
+			console.log("Received invalid chatMsg data! data.username was not a string!");
+			return;
+		}
+		
+		newChatMsg(username, msg);
+	})
+});
+
+// Client communication
+function newChatMsg(username, msg) {
+	console.log(`<${username}> ${msg}`);
+	
+	io.sockets.emit("chatMsg", {
+		username: username,
+		msg: msg
+	});
+}
+function sendRawMsg(text) {
+	console.log(text);
+	
+	io.sockets.emit("rawMsg", {
+		text: text
+	});
+}
